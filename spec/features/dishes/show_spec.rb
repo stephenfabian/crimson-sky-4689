f6acc0626eff45b6f4ce6516258e6bbece9ef703
Stephen Fabian <fabianstephen@gmail.com>
2022-09-20 12:05:44 -0600

    adds User Story 1

---
+++ spec/features/dishes/show_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+RSpec.describe 'Dishes Show Page Feature' do
+  describe 'When I visit a dishs show page' do
+    describe 'I see the dishs name and description' do
+      describe 'And I see a list of ingredients for that dish' do
+        it 'And I see the chefs name' do
+
+          chef1 = Chef.create(name: "Roger")
+
+          dish1 = chef1.dishes.create(name: "Pudding", description: "Terrible dessert")
+          dish2 = chef1.dishes.create(name: "Chips", description: "snack")
+
+          ingredient1 = dish1.ingredients.create(name: "Fat", calories: 9000)
+          ingredient2 = dish1.ingredients.create(name: "Milk", calories: 500)
+
+          visit "/dishes/#{dish1.id}"
+          expect(page).to have_content(dish1.name)
+          expect(page).to_not have_content(dish2.name)
+          expect(page).to have_content(dish1.description)
+          expect(page).to have_content(ingredient1.name)
+          expect(page).to have_content(ingredient2.name)
+          expect(page).to have_content(chef1.name)
+          save_and_open_page
+        end
+      end
+    end
+  end
+
+
+  describe 'When I visit a dishs show page' do
+    it 'I see the total calorie count for that dish' do
+
+      chef1 = Chef.create(name: "Roger")
+
+      dish1 = chef1.dishes.create(name: "Pudding", description: "Terrible dessert")
+      dish2 = chef1.dishes.create(name: "Chips", description: "snack")
+
+      ingredient1 = dish1.ingredients.create(name: "Fat", calories: 9000)
+      ingredient2 = dish1.ingredients.create(name: "Milk", calories: 500)
+
+
+      visit "/dishes/#{dish1.id}"
+      expect(page).to have_content("Calorie Count: 1400")
+    end
+  end
+end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -5,7 +5,10 @@ RSpec.describe Dish, type: :model do
     it {should validate_presence_of :name}
     it {should validate_presence_of :description}
   end
+
   describe "relationships" do
     it {should belong_to :chef}
+    it {should have_many(:ingredient_dishes)}
+    it {should have_many(:ingredients).through(:ingredient_dishes)}
   end
 end
